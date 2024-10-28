d57c132a01dbec1336c4d80638e2d58d10d3da88
SELCIYABLESSY S <150774900+Selciyablessy@users.noreply.github.com>
2024-10-28 14:16:53 +0530

    ForgotPasswordPage.jsx

--- MyBankCC/ForgotPasswordPage.jsx
+++ MyBankCC/ForgotPasswordPage.jsx
@@ -1,4 +1,3 @@
-// src/pages/ForgotPasswordPage.jsx
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import TextField from '@mui/material/TextField';
@@ -7,8 +6,10 @@ import Box from '@mui/material/Box';
 import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import CloseIcon from '@mui/icons-material/Close';
+import { useNavigate } from 'react-router-dom';
 
 const ForgotPasswordPage = ({ setIsForgotPassword }) => {
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
@@ -18,6 +19,7 @@ const ForgotPasswordPage = ({ setIsForgotPassword }) => {
   const onSubmit = (data) => {
     alert('Verification successful');
     setIsForgotPassword(false);
+    navigate('/home'); // Navigate to the login page after verification
   };
 
   return (
@@ -25,26 +27,19 @@ const ForgotPasswordPage = ({ setIsForgotPassword }) => {
       component="form"
       onSubmit={handleSubmit(onSubmit)}
       sx={{
+        bottom:'50px',
         maxWidth: '400px',
         padding: '20px',
         borderRadius: '8px',
         boxShadow: '0 4px 5px rgba(0,0,0,0.1)',
         backgroundColor: 'white',
         position: 'relative',
+        mx: 'auto', // Center horizontally
+        mt: '20vh', // Center vertically with margin
       }}
     >
-      <IconButton
-        onClick={() => setIsForgotPassword(false)}
-        sx={{
-          position: 'absolute',
-          top: '10px',
-          right: '10px',
-          color: 'gray',
-        }}
-      >
-        <CloseIcon />
-      </IconButton>
-      <Typography variant="h6" component="div" sx={{ mb: -1 }} textAlign={'center'}  color={'blue'}>
+    
+      <Typography variant="h6" component="div" sx={{ mb: 2, textAlign: 'top', color: 'blue' }}>
         Forgot Password Verification
       </Typography>
 
@@ -67,7 +62,7 @@ const ForgotPasswordPage = ({ setIsForgotPassword }) => {
         margin="normal"
         sx={{ mt: 1 }}
       />
-      <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 1 }}>
+      <Button type="submit" variant="contained" color="primary" fullWidth sx={{ mt: 2 }}>
         Verify
       </Button>
     </Box>
